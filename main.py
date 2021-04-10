c1609b70a88b6402b255b9eceb9e93d31fa38d87
patoshi77 <70019689+patoshi77@users.noreply.github.com>
2021-04-10 06:08:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,63 @@
-#!/usr/bin/env python3.8
-TOKEN='1762560403:AAGWkC57kJgn-LXbTVeo7E43wLS0M2St818'
-import os
-
-PORT = int(os.environ.get('PORT', 5000))
-# pylint: disable=W0613, C0116
-# type: ignore[union-attr]
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
-
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
+TOKEN='1762560403:AAGWkC57kJgn-LXbTVeo7E43wLS0M2St818'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-def start(update: Update, context: CallbackContext) -> None:
+def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
-
-def help_command(update: Update, context: CallbackContext) -> None:
+def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
-
-def echo(update: Update, context: CallbackContext) -> None:
+def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("TOKEN")
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
-
                           port=int(PORT),
-
                           url_path=TOKEN)
-
     updater.bot.setWebhook('https://wmcurrency.herokuapp.com/' + TOKEN)
-    
-    print('working...')
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-    
+
 if __name__ == '__main__':
     main()
